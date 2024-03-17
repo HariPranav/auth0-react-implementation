0dd2dd6e911309b3d804a15cbe4cb73def473a95
1nF0rmed <prkinformed@gmail.com>
2024-03-17 19:58:07 +0530

    moving to home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,179 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
+import Loading from "../components/Loading";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
-import Hero from "../components/Hero";
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [image, setImage] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [file, setFile] = useState(null); // New state variable for file
+  const { user,isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [token, setToken] = useState(null);
+  const [uploadResponse, setUploadResponse] = useState(null);
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [analyzeData, setAnalyzeData] = useState(null);
 
-const Home = () => (
-  <Fragment>
-    <Hero />
-  </Fragment>
-);
+  useEffect(() => {
+    const fetchToken = async () => {
+      const fetchedToken = await getAccessTokenSilently();
+      setToken(fetchedToken);
+    };
+
+    fetchToken();
+  }, [getAccessTokenSilently]);
+
+  const handleImageUpload = async (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+  
+    reader.onload = async () => {
+      try {
+        setImage(reader.result);
+      } catch (error) {
+        console.error("Error reading EXIF data:", error);
+      }
+    };
+  
+    if (file) {
+      reader.readAsDataURL(file);
+      setFile(file); // Set the file state variable
+    }
+  };
+
+  const handleUpload = async () => { // New function to handle upload
+    if (file) {
+      setIsLoading(true);
+      try {
+        // Create a FormData instance
+        const formData = new FormData();
+        // Append the file to the form data
+        formData.append("file", file);
+  
+        // Send a POST request to your backend
+        const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/upload-file`, {
+          method: "POST",
+          body: formData,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+  
+        if (!response.ok) {
+          throw new Error("Error uploading file");
+        }
+
+        const responseData = await response.json();
+        setUploadResponse(responseData);
+        setIsUploaded(true);
+      } catch (error) {
+        console.error("Upload failed:", error);
+      } finally {
+        setIsLoading(false); // End loading
+      }
+    }
+  };
+
+  const handleAnalyze = async () => {
+    if (uploadResponse) {
+      setIsLoading(true);
+      try {
+        const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/analyze`, {
+          method: "POST",
+          body: JSON.stringify({ data: uploadResponse.data }),
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Error analyzing file");
+        }
+
+        const analyzeData = await response.json();
+        setAnalyzeData(analyzeData); // Log the analyze data or do something with it
+      } catch (error) {
+        console.error("Analyze failed:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  const handleReset = () => {
+    setIsUploaded(false);
+    setUploadResponse(null);
+    setAnalyzeData(null);
+    setImage(null);
+  };
+  
+  return (
+    <Fragment>
+      <div>
+        {isLoading && <Loading />}
+        {isUploaded ? (
+          <>
+            {image && (
+              <div>
+                Selected Image:
+                <img src={image} alt="Uploaded" style={{ maxWidth: "100%" }} />
+              </div>
+            )}
+            <button onClick={handleAnalyze}>Analyze</button>
+            <button onClick={handleReset}>Upload Another</button>
+            {analyzeData && (
+              <div>
+                Products that can be made:
+              </div>
+            )}
+            {analyzeData && analyzeData.products.map((product, index) => (
+              <div key={index}>
+                <h2>{product.name}</h2>
+                <h3>Required Items:</h3>
+                <ul>
+                  {product.items.map((item, itemIndex) => (
+                    <li key={itemIndex}>{item}</li>
+                  ))}
+                </ul>
+                <h3>Steps:</h3>
+                <ol>
+                  {product.steps.map((step, stepIndex) => (
+                    <li key={stepIndex}>{step}</li>
+                  ))}
+                </ol>
+              </div>
+            ))}
+          </>
+        ) : (
+          <>
+            <h1 className="mb-4">Save The Trash</h1>
+            <p className="lead">
+              Share a picture of your trash and we can help you find ways to reuse it!
+            </p>
+            <div>
+            {image && (
+              <div>
+                Selected Image:
+                <img src={image} alt="Uploaded" style={{ maxWidth: "100%" }} />
+              </div>
+            )}
+            </div>
+            <div>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
+                style={{ display: "block", margin: "0 auto" }}
+              />
+              <button onClick={handleUpload}>Upload</button>
+            </div>
+          </>
+        )}
+      </div>
+    </Fragment>
+  );
+}
 
 export default Home;
