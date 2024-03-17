9879db2120ca37d78c8eb8a1d36dec9c0e9db4d8
1nF0rmed <prkinformed@gmail.com>
2024-03-17 16:37:31 +0530

    add access token

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/logo.svg";
+import Loading from './Loading';
 import exifr from "exifr";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
 const Hero = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [image, setImage] = useState(null);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
+  const [file, setFile] = useState(null); // New state variable for file
+  const { getAccessTokenSilently } = useAuth0();
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      const fetchedToken = await getAccessTokenSilently();
+      setToken(fetchedToken);
+    };
+
+    fetchToken();
+  }, [getAccessTokenSilently]);
 
   const handleImageUpload = async (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
-
+  
     reader.onload = async () => {
       try {
         // Extracting latitude and longitude using exifr library
@@ -22,9 +37,39 @@ const Hero = () => {
         console.error("Error reading EXIF data:", error);
       }
     };
-
+  
     if (file) {
       reader.readAsDataURL(file);
+      setFile(file); // Set the file state variable
+    }
+  };
+
+  const handleUpload = async () => { // New function to handle upload
+    if (file) {
+      setIsLoading(true);
+      try {
+        // Create a FormData instance
+        const formData = new FormData();
+        // Append the file to the form data
+        formData.append("file", file);
+  
+        // Send a POST request to your backend
+        const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/upload-file`, {
+          method: "POST",
+          body: formData,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+  
+        if (!response.ok) {
+          throw new Error("Error uploading file");
+        }
+      } catch (error) {
+        console.error("Upload failed:", error);
+      } finally {
+        setIsLoading(false); // End loading
+      }
     }
   };
 
@@ -41,7 +86,8 @@ const Hero = () => {
           onChange={handleImageUpload}
           style={{ display: "block", margin: "0 auto" }}
         />
-        {image && (
+        <button onClick={handleUpload}>Upload</button>
+        {isLoading ? <Loading /> : image && (
           <div>
             <img src={image} alt="Uploaded" style={{ maxWidth: "100%" }} />
             {(latitude && longitude) ? (
